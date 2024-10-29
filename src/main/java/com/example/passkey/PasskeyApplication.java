10186d09fb1bf420407cd1c3b0a8ca006b525156
fromitive <fromitive@gmail.com>
2024-10-29 23:34:33 +0900

    init project

---
+++ src/main/java/com/example/passkey/PasskeyApplication.java
@@ -0,0 +1,13 @@
+package com.example.passkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasskeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasskeyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/passkey/PasskeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.passkey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasskeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
